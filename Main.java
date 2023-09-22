934d811d32b0b7065f84167855f1627455d6be9f
officallyJoshua <145042827+officallyJoshua@users.noreply.github.com>
2023-09-22 09:23:57 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        double pricePurchase = 50;
+        double salesTax =  0.05 * pricePurchase;
+
+        System.out.println("price purchase: $" + pricePurchase);
+        System.out.println("sales tax: $" + salesTax);
+    }
+}
